59cee90889bd945ab847c0cd0a8a16cd57b7cb84
Gabriel Sollero <sollero.gms@gmail.com>
2024-08-21 20:11:13 -0300

    feat: list input work item variables in task

--- tasks.py
+++ tasks.py
@@ -1,6 +1,12 @@
+import logging
+from RPA.Robocorp.WorkItems import WorkItems
 from robocorp.tasks import task
 
 @task
 def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+    library = WorkItems()
+    library.get_input_work_item()
+
+    variables = library.get_work_item_variables()
+    for variable, value in variables.items():
+        logging.info("%s = %s", variable, value)
